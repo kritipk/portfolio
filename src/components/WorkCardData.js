4f87a4b58c4ae0c60c40a2d526d991f494e4d2de
kriti <kritipk0815@gmail.com>
2023-06-21 14:11:32 +0530

    comment project

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -3,23 +3,17 @@ import i2 from "../assets/p2.gif"
 import i3 from "../assets/p3.gif"
 
 const ProjectCardData = [
-    {
-        imgsrc: i1,
-        title: "GUI-CALCULATOR",
-        text: "The GUI based calculator in Python is a simple calculator application that performs basic mathematical operations such as addition, subtraction, multiplication, and division. The application provides a user-friendly interface to enter the numbers and perform calculations.",
-        source: "https://github.com/UnnatiSamaiyar/GUI-based-Python-Calculator.git"
-    },
-    {
-        imgsrc: i2,
-        title: "ATM-MANAGER",
-        text: "Project named ATM Manager uses Python_My-SQL Interface. User enters the data like adding a new user or creating a new account and the entered data is stored in MySQL Database.",
-        source: "https://github.com/UnnatiSamaiyar/ATM-Manager.git"
-    },
+    // {
+    //     imgsrc: i2,
+    //     title: "ATM-MANAGER",
+    //     text: "Project named ATM Manager uses Python_My-SQL Interface. User enters the data like adding a new user or creating a new account and the entered data is stored in MySQL Database.",
+    //     source: ""
+    // },
     {
         imgsrc: i3,
         title: "CAMERA AI-MODEL",
         text: "The Camera AI Model for Automated Attendance Using Face Recognition is a software application that uses computer vision and machine learning to identify students and take their attendance automatically using a camera. The application will use face recognition algorithms to identify students and compare them with the registered faces in the database.",
-        source: "https://github.com/UnnatiSamaiyar/Camera-AI-Model.git"
+        source: "https://github.com/kritipk/Camera-Model"
     },
 ];
 
